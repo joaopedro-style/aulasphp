858b0fe6cf68d619306d4a2801dd1e7063d9f445
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-04 11:34:12 -0300

    condicionais

---
+++ 05-condicionais.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Condicionais</title>
+</head>
+
+<body>
+    <h1>Estruturas condicionais</h1>
+    <hr>
+
+    <h2>Simples</h2>
+
+    <h2>Composta</h2>
+
+    <h2>Encadeada</h2>
+</body>
+
+</html>
