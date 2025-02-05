f78e0b4876374ce87356e768fb51d17285383e4d
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-05 11:55:56 -0300

    exercício 03 corrigido

--- exercicios/exercicio03.php
+++ exercicios/exercicio03.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 03</title>
 </head>
-    <h1>Exercíco 03 (Condicionais)</h1>
-    <hr>
+<h1>Exercíco 03 (Condicionais)</h1>
+<hr>
 
 <?php
 $compra = 7000;
@@ -24,8 +24,18 @@ if ($compra > 5000) {
 
 $valorFinal = $compra - $compra * $desconto;
 ?>
+
+<p>Valor da compra (sem desconto): R$
+    <?= number_format($valorFinal, 2, ",", ".") ?>
+</p>
+
 <p>Valor final da compra é: R$
-    <?=number_format($valorFinal, 2, ",", ".")?></p>
+    <?= number_format($valorFinal, 2, ",", ".") ?>
+</p>
+
+<p>Foi dado um desconto de:
+    <?= $desconto * 100 ?>% (<?= $compra - $valorFinal ?>)
+</p>
 
 <body>
 
