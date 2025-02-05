c381be2868c6497b9f93eeaaed6957e7bce3713a
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-05 08:34:30 -0300

    Condicionais composta

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -12,20 +12,36 @@
     <hr>
 
     <h2>Simples usando <code>if</code></h2>
-<?php
-$numero = 5;
+    <?php
+    $numero = 5;
 
-if($numero > 1){
-    echo "<p>$numero é maior que 1</p>";
+    if ($numero > 1) {
+        echo "<p>$numero é maior que 1</p>";
+    }
+
+    // Sintaxe omitindo as chaves
+    if ($numero > 1) echo "<p>$numero é maior que 1</p>";
+    ?>
+
+    <hr>
+    <h2>Composta</h2>
+<?php
+$produto = "Ultrabook Asus";
+$qtdEmEstoque = 1; // o que temos
+$qtdCritica = 5; // mínimo necessário
+
+echo "<h3>$produto</h3>";
+echo "<h4>Quantidade em estoque: $qtdEmEstoque</h4>";
+
+if ($qtdEmEstoque < $qtdCritica) {
+    echo "<p>É necessario comprar!</p>";
+} else {
+    echo "<p>Estoque normal</p>";
 }
 
-// Sintaxe omitindo as chaves
-if($numero > 1) echo "<p>$numero é maior que 1</p>";
 ?>
 
-
-    <h2>Composta</h2>
-
+    <hr>
     <h2>Encadeada</h2>
 </body>
 
