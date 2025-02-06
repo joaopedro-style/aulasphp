387ecb63f6f233f822338c4933fbee8968f3e173
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-06 08:48:16 -0300

    Cria exemplo !

--- 06-operadores-logicos.php
+++ 06-operadores-logicos.php
@@ -44,9 +44,21 @@ if ($clienteVIP || $temCupom) {
 
 ?>
 
+<hr>
 
     <h2>! (NÃO/NOT)</h2>
+    <p><i>É uma inversão de lógica: VERDADEIRO vira <b>FALSO</b>, FALSO vira <b>VERDADEIRO</b></i></p>
+<?php
+/* Se o usuário NÃO ESTIVER logado, exibir o link/botão de LOGIN. Caso contrário, exibir uma saudação. */
+$usuarioLogado = false;
 
+if (!$usuarioLogado) {
+    echo "<a herf=\"login.php\">Login</a>";
+} else {
+    echo "<span>Bem-vindo ao sistema!</span>";
+}
+
+?>
 
 
 </body>
