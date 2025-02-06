75271a0fbe8def749a7121c668a4dbbf0ac2b05d
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-06 09:53:02 -0300

    Adiciona exemplo FOR

--- 07-loops.php
+++ 07-loops.php
@@ -36,8 +36,17 @@ do {
     $j++;
 } while ($j <= 3);
 ?>
-    
+    <hr>
+
     <h2>FOR (PARA)</h2>
     <p>Executa ações por uma <b>quantidade determinada</b> de vezes.</p>
+<?php
+for( $i = 1; $i <= 10; $i++ ){
+?>
+    <p><code>i</code> vale: <b><?=$i?></b></p>
+<?php
+}
+?>
+
 </body>
 </html>
