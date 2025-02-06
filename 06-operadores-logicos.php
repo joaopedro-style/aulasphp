93d45588678143ce1d9e16823b1eb32fc9a7ff0d
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-06 08:25:29 -0300

    Cria Arquivo operador lógico

---
+++ 06-operadores-logicos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Operadores Lógicos</title>
+</head>
+
+<body>
+    <h1>Relembrando Operadores Lógicos</h1>
+    <hr>
+
+    <h2>&& (E/AND)</h2>
+
+
+    <!-- osínbolo | é chamado de pipe -->
+    <h2>|| (Ou/OR)</h2>
+
+    <h2>! (NÃO/NOT)</h2>
+
+
+
+</body>
+
+</html>
