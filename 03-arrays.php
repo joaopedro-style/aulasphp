5ba813f5458afd0ff8390599b5e7af181f9d6795
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-10 11:27:07 -0300

    Adiciona bootstrap

--- 03-arrays.php
+++ 03-arrays.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - Arrays (Vetores e Matrizes)</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
    <h1>Trabalhando com arrays</h1>
    <hr>
    
@@ -27,15 +29,15 @@ define("UNIDADES", ["Penha", "Tatuapé"]);
 const FRUTAS = ["Morango", "Abacaxi"];
 ?> 
     <h3>Acessando os dados</h3>
-    <ul>
-        <li>Banda que mais curto: <?=$bandas[1]?></li>
-        <li>Em breve vou lançar um curso de <?=$bandas[2]?></li>
-        <li>Quero comer <?=$comidas[0]?></li>
-        <li>Estamos no senac <?=UNIDADES[0]?></li>
-        <li>Vou fazer uma Vitamina de <?=FRUTAS[1]?></li>
+    <ul class="list-group">
+        <li class="list-group-item">Banda que mais curto: <?=$bandas[1]?></li>
+        <li class="list-group-item">Em breve vou lançar um curso de <?=$bandas[2]?></li>
+        <li class="list-group-item">Quero comer <?=$comidas[0]?></li>
+        <li class="list-group-item">Estamos no senac <?=UNIDADES[0]?></li>
+        <li class="list-group-item">Vou fazer uma Vitamina de <?=FRUTAS[1]?></li>
     </ul>
 
-    <h2>Arrays associativos</h2>
+    <h2>Arrays associativos</h2>    
 <?php
 $curso = [
     "titulo" => "Gastronomia",
@@ -92,6 +94,8 @@ $planoDeEstudos = [
     <pre> <?=var_dump($bandas)?> </pre>
     <pre> <?=var_dump($curso)?> </pre>
 
-
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
--- exercicios/exercicio04.php
+++ exercicios/exercicio04.php
@@ -4,23 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 04</title>
-    <style>
-        table {
-            width: 70%;
-            margin: 20px auto;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 10px;
-            border: 1px solid;
-            text-align: center;
-        }
-        th {
-            background-color: darkgrey;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <style> -->
+        <!-- table { -->
+            <!-- width: 70%; -->
+            <!-- margin: 20px auto; -->
+            <!-- border-collapse: collapse; -->
+        <!-- } -->
+        <!-- th, td { -->
+            <!-- padding: 10px; -->
+            <!-- border: 1px solid; -->
+            <!-- text-align: center; -->
+        <!-- } -->
+        <!-- th { -->
+            <!-- background-color: darkgrey; -->
+        <!-- } -->
+    <!-- </style> -->
 </head>
 <body>
+    <div class="container">
     <h1>Exercício 04 (Loop e Estrutura de dados)</h1>
     <hr>
 
@@ -34,7 +36,7 @@ $linguagens =[
     "JAVA" => "Java é uma linguagem de programação e plataforma de computação"
 ];
 ?>
-<table>
+<table class="table table-dark table-striped">
         <tr>
             <th>ID</th>
             <th>Linguagem</th>
@@ -89,7 +91,7 @@ $linguagens = [
     ],
 ];
 ?>
-<table>
+<table class="table table-striped">
         <tr>
             <th>ID</th>
             <th>Linguagem</th>
@@ -108,6 +110,8 @@ foreach ($linguagens as $linguagem ) {
 }
 ?>
 </table>
+</div>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
