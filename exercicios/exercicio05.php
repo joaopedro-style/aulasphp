fa26bc6686cf7b6d8d1f75e7f699f373dbf71598
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-11 14:28:37 -0300

    exercício 05

--- exercicios/exercicio05.php
+++ exercicios/exercicio05.php
@@ -1,42 +1,59 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 05</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-  
-    <h1>Exercício 05 (Funções)</h1>
-    <hr>
-    <?php
-    $nota1 = 7;
-    $nota2 = 10;
-    $nota3 = 4;
+    <div class="container">
+        <h1>Exercício 05 (Funções)</h1>
 
-    $media = ($nota1 + $nota2 + $nota3) / 3
-    ?>
+        <hr>
 
-    <p>A média é : <?=$media?></p>
+        <?php
+        $nota1 = 7;
+        $nota2 = 10;
+        $nota3 = 4;
 
-    <?php
-    if ($media >= 7) { ?>
-        <p>Aprovado!</p>
-    <?php }else { ?>
-        <p>Reprovado!</p>
-    <?php } ?>
+        $media = ($nota1 + $nota2 + $nota3) / 3
+        ?>
 
-    <?php
-    function calcularMedia():string{
-        return "A média é: ";
-    }
-    ?>
+        <p>A média é : <?= $media ?></p>
 
-    <p>Média é: <?=calcularMedia()?></p>
-    <p>Média é: <?=calcularMedia()?></p>
+        <?php
+        if ($media >= 7) { ?>
+            <p class="bg-primary">Aprovado!</p>
+        <?php } else { ?>
+            <p class="bg-danger">Reprovado!</p>
+        <?php } ?>
 
+        <?php
+        function calcularMedia($nota1, $nota2, $nota3)
+        {
+            return ($nota1 + $nota2 + $nota3) / 3;
+        }
+        ?>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <p>Média: <?= $media ?></p>
+        <p> Situação: <?= situacaoDoAluno($media) ?></p>
+
+        <?php
+        function situacaoDoAluno(float $media): string
+        {
+            if ($media >= 7) {
+                return "Aprovado!";
+            } else {
+                return "Reprovado!";
+            }
+        }
+        ?>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+
 </html>
