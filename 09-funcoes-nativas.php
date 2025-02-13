47f582ff1a08acf9ad13ce1347b3f3fd53af0038
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-13 08:57:37 -0300

    Adiciona explode

--- 09-funcoes-nativas.php
+++ 09-funcoes-nativas.php
@@ -35,6 +35,18 @@
     ?>
         <p><?=$fraseFeia?></p>
         <p><?=$fraseBunitinha?></p>
+
+        <hr>
+
+        <h3><code>explode()</code></h3>
+        <p>Transformar uma string em um array</p>
+    <?php
+    $textosLinguagens = "HTML,CSS,JS,PHP,SQL";
+    $arrayLinguagens = explode(",", $textosLinguagens);
+    ?>
+    <pre><?=var_dump($textosLinguagens)?></pre>
+    <pre><?=var_dump($arrayLinguagens)?></pre>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
