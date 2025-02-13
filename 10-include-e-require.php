6778e284ce5e692ab38dfb807f9e1f90f0ff2f9d
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-13 11:32:41 -0300

    Acessa recursos com include

--- 10-include-e-require.php
+++ 10-include-e-require.php
@@ -1,3 +1,4 @@
+<?php include "recursos.php"; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,6 +12,19 @@
     <div class="container">
         <h1>Modularização e Inclusão de Recursos</h1>
         <hr>
+        <h2> <?=ESCOLA?> </h2>
+        <p>Estamos fazendo o curso de <?=$curso?> </p>
+        <ul>
+            <!-- Acessando o array -->
+            <?php foreach ($tecnologias as $tecnologia) {?>
+                <li><?=$tecnologia?></li>
+            <?php } ?>
+        </ul>
+
+        <hr>
+
+        <p>O aluno Fulano de Tal tem 20 anos e é <?=verificarIdade(20)?> de idade</p>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
