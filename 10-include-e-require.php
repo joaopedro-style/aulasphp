02416aa50211535e633abee367e0fb58bc2cc747
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-13 11:40:12 -0300

    Inclui recursos HTML

--- 10-include-e-require.php
+++ 10-include-e-require.php
@@ -25,6 +25,14 @@
 
         <p>O aluno Fulano de Tal tem 20 anos e é <?=verificarIdade(20)?> de idade</p>
 
+        <hr>
+
+        <article>
+            <h2>Título qualquer...</h2>
+            <!-- Incluindo fragmento de HTML -->
+            <?php include "textos.html";?>
+        </article>
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
