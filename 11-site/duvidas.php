936e18d5727eb4c4df617de799fbc1a2511ae412
joaopedro-style <joaopedro18231@hotmail.com>
2025-02-14 08:37:57 -0300

    Cria duvidas.php

---
+++ 11-site/duvidas.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site com PHP</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Site com PHP</h1>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="cursos.php">Cursos</a>
+                <a href="duvidas.php">Dúvidas</a>
+            </nav>
+        </header>
+
+        <main>
+            <h2>Página de dúvidas</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt ad repellendus ipsum sunt illum veritatis error sapiente cupiditate facilis velit voluptates omnis nam in explicabo, aut neque voluptatum.</p>
+
+            <h3>Principais dúvidas</h3>
+            <details>
+                <summary>Como se cadastrar?</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </details>
+            <details>
+                <summary>Os cursos são gratuitos?</summary>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam itaque a illo natus eum aspernatur expedita.</p>
+            </details>
+        </main>
+        <footer>
+            <p>Site criado para o curso Téc. Informática para internet</p>
+        </footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
